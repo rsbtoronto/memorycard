bbe56e877ee3b9ebaf3d660b2d6f08debcfa9695
Samaga, Raghu <rsbtoronto@gmail.com>
2020-07-13 05:42:52 -0400

    changes to add new button and features

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,21 +5,93 @@ import NavBar from "./NavBar";
 
 
 class Game extends React.Component {
-  state = {
+  constructor(props) {
+    super(props);
+  this.state = {
    BeginGame: true,
     turns: 0,
     score: 0,
   };
+  this.checkIfClicked = this.checkIfClicked.bind(this);           
 
+}
+images = [];
+turn = 0;
+
+checkIfClicked= (e)=> {        
+      // set the state and increment the counter
+       
+      this.turn = this.turn + 1; 
+    //this.setState({turns: this.turn});
+    
+      let image = e.target;      
+     
+      if (image.getAttribute("check") === "match") {        
+        return;
+      }      
+      
+      if (image !== this.images[0]) {
+        this.switch(image);
+       this.images.push(image);       
+      } 
+      else {        
+        this.switch(image);
+        this.images = [];        
+      }
   
+      if (this.images.length === 2) {        
+        if (!this.checkName(this.images[0], this.images[1])) {
+          setTimeout(() => {
+          this.switch(this.images[0]);
+          this.switch(this.images[1]);
+          this.images.shift();
+          this.images.shift();
+          console.log("not eqaul", this.images);
+        }, 1200);        
+      } else { this.images = [];}
+      } 
+    };
+    
+    /* Reset for New Game */
+    reset(){    
+     console.log("reset")
+      let reset = document.getElementsByClassName("card");
+      for (let i = 0; i < reset.length; i++) {
+        reset[i].classList.add("card-blank");
+        reset[i].setAttribute("check", "false");                     
+    }
+    };
 
+    checkName = (image1, image2) => {
+      if (image1.getAttribute("id") === image2.getAttribute("id")) {
+        image1.setAttribute("check", "match");
+        image2.setAttribute("check", "match");        
+        return true;
+      }
+      return false;
+    };
+  
+    switch = (target) => {
+      if (target.getAttribute("check") === "true") {
+        target.setAttribute("check", "false");
+        target.classList.add("card-blank");
+      } else {
+        console.log("switch else");
+        target.setAttribute("check", "true");
+        target.classList.remove("card-blank");
+      }
+    };
+    
   render() {
-    const { BeginGame, turns, score} = this.state;
-
+    const { turns, score} = this.state;
+    
     return (
-      <div className="game">
-        <NavBar turns={turns} score={score} /> 
-        <div className="cards"><MemoryCards  BeginGame={this.handleBeginGame}/></div>        
+      <div className="game">        
+       { /*<NavBar turns={turns} score={score} /> */}
+        <div className="cards"><MemoryCards checkIfClicked={this.checkIfClicked}  /></div>      
+        <div className="button-group">
+               <button  type="submit" className="btn btn-primary" onClick={this.reset}>New Game</button> 
+           </div>
       </div>
     );
   }
--- src/components/MemoryCards.js
+++ src/components/MemoryCards.js
@@ -1,71 +1,11 @@
-import React from 'react';
+import React   from 'react';
 import {Cards} from './Cards';
 import '../css/MemoryCards.css';
 
-class MemoryCards extends React.Component {
-    images = [];
-    handleClick = (e) => {      
-      let image = e.target;
-  
-      if (image.getAttribute("check") === "match") {
-        return;
-      }
-      console.log(image, this.images[0]);
-  
-      if (image !== this.images[0]) {
-        this.switch(image);
-        this.images.push(image);
-      } else {
-        this.switch(image);
-        this.images = [];
-      }
-  
-      if (this.images.length > 2) {
-        if (!this.checkName(this.images[0], this.images[1])) {
-          this.switch(this.images[0]);
-          this.switch(this.images[1]);
-          this.images.shift();
-          this.images.shift();
-        } else {
-          this.images.shift();
-          this.images.shift();
-        }
-      }
-      let allPictures = document.getElementsByClassName("card-blank");
-      if (allPictures.length < 1) { 
-        console.log("allPictures.length", allPictures.length);
-       // this.props.endGame(true);
-        let reset = document.getElementsByClassName("card");
-        for (let i = 0; i < reset.length; i++) {
-          reset[i].classList.add("card-blank");
-          reset[i].setAttribute("check", "false");
-          this.images = [];
-        }
-      }
-    };
-  
-    checkName = (image1, image2) => {
-      if (image1.getAttribute("id") === image2.getAttribute("id")) {
-        image1.setAttribute("check", "match");
-        image2.setAttribute("check", "match");
-        return true;
-      }
-      return false;
-    };
-  
-    switch = (target) => {
-      if (target.getAttribute("check") === "true") {
-        target.setAttribute("check", "false");
-        target.classList.add("card-blank");
-      } else {
-        console.log("switch else");
-        target.setAttribute("check", "true");
-        target.classList.remove("card-blank");
-      }
-    };
-    render() {
-      return (
-        <div className="cards">
+    const MemoryCards = props => (     
+             
+              
+        <div className="cards">           
           {Cards
             .sort(() => Math.random() - 0.5)
             .map((element) => {
@@ -76,13 +16,12 @@ class MemoryCards extends React.Component {
                   id={element.id}
                   style={{ background: `url(${element.pic})` }}
                   check="false"
-                  onClick={this.handleClick}
+                  onClick={(e) => props.checkIfClicked(e)}                 
                 />
               );
-            })}
-        </div>
+            })}                             
+           
+        </div>         
       );
-    }
-  }
 
   export default MemoryCards;
